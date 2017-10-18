9c9a8c778b908f6965b25e54e770810d57ed1eeb
Victor Teodoro <victor.teodoro@pagar.me>
2017-10-18 06:21:11 -0200

    Terminei a demo da Ambev com GET simulando um push

--- src/pages/payment/index.js
+++ src/pages/payment/index.js
@@ -18,24 +18,57 @@ export default {
     },
     onMount () {
 	const router = this.$router;
-	const handshakeButton = this.$refs['handshake-button'].el;
-	const printArea = this.$refs['print-area'];
+	let authorizeButton = document.getElementById('authorize-button');
+	let valorDoPedido = document.getElementById('valor-do-pedido');
+	const filipetaPedido = this.$refs['filipeta-pedido'];
+	const filipetaParceiro = this.$refs['filipeta-parceiro'];
+	const filipetaAmbev = this.$refs['filipeta-ambev'];
+
+	// Make buttons invisible
+	valorDoPedido.style.visibility = 'hidden';
+
+	// Define os onclicks
+	authorizeButton.onclick = onclicks;
+
+	function onclicks() {
+	    filipetaParceiro.print(this.printObject, err => {
+		if (err === undefined) {
+		    // Make buttons invisible
+		    authorizeButton.innerHTML = 'Via da Ambev';
+		    authorizeButton.onclick = function () {
+			filipetaAmbev.print(this.printObject, err => {
+			    if (err === undefined) {
+				// Make buttons invisible
+				authorizeButton.innerHTML = 'Sucesso!';
+			    } else {
+				console.log('Printing error!');
+			    }
+			});
+		    }
+		} else {
+		    console.log('Printing error!');
+		}
+	    });
+	}
 
 	setInterval(function(){
 	    let xhttp = Tools.configureHttpReq('ravTotal advance', 'https://solutions-api.herokuapp.com/garantia_handshake')
 	    xhttp.onreadystatechange = response => {
-		console.log('Estou aqui!');
+		//console.log('Estou aqui!');
 		if (xhttp.readyState === 4) {
 		    if (xhttp.status === 200) {
 			// get body data
 			let needsHandshake = JSON.parse(response.srcElement.response).needs_handshake;
-			console.log(needsHandshake);
+			//console.log(needsHandshake);
 
 			if(needsHandshake) {
 			    success = true;
-			    printArea.print(this.printObject, err => {
+			    filipetaPedido.print(this.printObject, err => {
 				if (err === undefined) {
-				    //router.push('/');
+				    // Make buttons invisible
+				    valorDoPedido.style.visibility = 'visible';
+				    authorizeButton.innerHTML = 'Autoriza Ambev?';
+				    authorizeButton.onclick = onclicks;
 				} else {
 				    console.log('Printing error!');
 				}
